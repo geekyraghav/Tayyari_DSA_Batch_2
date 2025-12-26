d2451f009f832a7af70d39d5eb3e0ae42dcfc064
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2025-12-26 22:04:41 +0530

    Add files via upload

---
+++ BinaryTrees/SymmetricTree.java
@@ -0,0 +1,33 @@
+package BinaryTrees;
+
+public class SymmetricTree {
+    public boolean isSymmetric(Node root) {
+        return helper(root.left,root.right);
+    }
+
+    private boolean helper(Node p, Node q) {
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.data != q.data) return false;
+        return helper(p.left,q.right) && helper(p.right,q.left);
+    }
+
+//    public boolean isSymmetric(Node root) {
+//        mirror(root.right);
+//        return isIdentical(root.left,root.right);
+//    }
+//    public void mirror(Node root) {
+//        if(root==null) return;
+//        Node temp = root.left;
+//        root.left = root.right;
+//        root.right = temp;
+//        mirror(root.left);
+//        mirror(root.right);
+//    }
+//    public boolean isIdentical(Node p, Node q) {
+//        if(p==null && q==null) return true;
+//        if(p==null || q==null) return false;
+//        if(p.data != q.data) return false;
+//        return isIdentical(p.left,q.left) && isIdentical(p.right,q.right);
+//    }
+}

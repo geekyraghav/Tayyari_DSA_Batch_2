57109ca3e125c81bf18b215f7989f0e6a647eaf3
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-02-16 22:27:00 +0530

    Add files via upload

---
+++ Graphs/NumberOfProvinces.java
@@ -0,0 +1,31 @@
+package Graphs;
+import java.util.*;
+public class NumberOfProvinces {
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int n) {
+        boolean[] vis = new boolean[n];
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                bfs(i,adj,vis);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static void bfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
+        int n = vis.length;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        vis[node] = true;
+        while(q.size() > 0){
+            int front = q.remove();
+            for(int i=0;i<n;i++){
+                if(i!=front && !vis[i] && adj.get(front).get(i)==1){ // adj[front][i]
+                    q.add(i);
+                    vis[i] = true;
+                }
+            }
+        }
+    }
+}
